60f524c3712b0152309df3f6648a9c91fca14a31
iamlyongift <chukwumagift6036@gmail.com>
2024-11-20 12:27:06 +0100

    modified login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,12 +6,14 @@ export default function AdminLogin() {
   // State to capture email and password inputs
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false); // New loading state
   const [error, setError] = useState(""); // To display error messages
   const navigate = useNavigate(); // Hook to redirect after login
 
   // Handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault(); // Prevent form from refreshing the page
+    setLoading(true); // Set loading to true when submitting
 
     const baseUrl = "https://wheelhouse.onrender.com";
     try {
@@ -20,16 +22,13 @@ export default function AdminLogin() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ username, password }), // Send email and password
+        body: JSON.stringify({ username, password }), // Send username and password
       });
 
       const data = await response.json(); // Parse the response
 
       if (response.ok) {
-        localStorage.setItem("token", data.token);
-        // Login successful, redirect to the dashboard or another page
-        // You can also store the token in localStorage if provided by the backend
-        // localStorage.setItem("token", data.token);
+        localStorage.setItem("token", data.token); // Save token in localStorage
         navigate("/"); // Redirect to the dashboard or home page
       } else {
         // Login failed, display error message
@@ -37,6 +36,8 @@ export default function AdminLogin() {
       }
     } catch (error) {
       setError("An error occurred. Please try again later.", error);
+    } finally {
+      setLoading(false); // Set loading to false when the request completes
     }
   };
 
@@ -45,7 +46,7 @@ export default function AdminLogin() {
       <div className="login-container">
         <div className="login-form">
           <h2>Login</h2>
-          <h1>Welcome Back Admin Dozie</h1>
+          <h1>Welcome Back Admin</h1>
           <div className="login-con">
             <form onSubmit={handleSubmit}>
               <input
@@ -74,9 +75,10 @@ export default function AdminLogin() {
               </div>
               {error && <p className="error-message">{error}</p>}{" "}
               {/* Display errors */}
-              <button type="submit" className="login-button">
-                LOGIN
+              <button type="submit" className="login-button" disabled={loading}>
+                {loading ? "Logging in..." : "LOGIN"}
               </button>
+              {loading && <div className="loader"></div>}
             </form>
           </div>
         </div>
